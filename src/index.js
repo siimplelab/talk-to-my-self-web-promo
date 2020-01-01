37b5322d5a65ca09f3d33036712e3cde7f23f4f6
hyungsub08 <hyungsub08@gmail.com>
2020-01-01 21:44:01 +0900

    webpack change

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
 
     // Lodash, currently included via a script, is required for this line to work
     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.innerHTML = "Hello Webpack";
+    element.innerHTML = "Hello Webpack Hi";
     element.classList.add('hello');
 
     return element;
